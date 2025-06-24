79923cdbca9e9b171a2cb6a845394b62c5c43d08
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-06-24 18:51:36 -0400

    Don't start if required environment variables missing

--- const.py
+++ const.py
@@ -9,5 +9,7 @@ LOG_FORMAT = "%(asctime)-20s %(levelname)-9s [%(name)s] %(message)s"
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 DEFAULT_LOGLEVEL = logging.INFO
 
+REQUIRED_VARS: list[str] = ["LLDAP_USERNAME", "LLDAP_PASSWORD", "AUTHELIA_URL"]
+
 DEFAULT_LLDAP_CONFIG = "/app/lldap_config/config.toml"
 DEFAULT_LLDAP_HTTPURL = "http://lldap:17170"
