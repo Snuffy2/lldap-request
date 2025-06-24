1e308dd4703d5b7ac39043096c0fc6a685ebc466
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-06-24 17:36:53 -0400

    Show lldap-request version (#10)

--- const.py
+++ const.py
@@ -1,3 +1,10 @@
 """Constants for lldap-request."""
 
+import logging
+
+# Will auto-update based on GitHub release
 VERSION = "v0.0.3"
+
+LOG_FORMAT = "%(asctime)-20s %(levelname)-9s [%(name)s] %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+DEFAULT_LOGLEVEL = logging.INFO
