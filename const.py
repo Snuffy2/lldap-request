3c084eda9373115cb9e5f1aa33ea21804db47e22
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-06-24 18:27:01 -0400

    Give some environment variables defaults (#12)

--- const.py
+++ const.py
@@ -8,3 +8,6 @@ VERSION = "v0.0.3"
 LOG_FORMAT = "%(asctime)-20s %(levelname)-9s [%(name)s] %(message)s"
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 DEFAULT_LOGLEVEL = logging.INFO
+
+DEFAULT_LLDAP_CONFIG = "/app/lldap_config/config.toml"
+DEFAULT_LLDAP_HTTPURL = "http://lldap:17170"
