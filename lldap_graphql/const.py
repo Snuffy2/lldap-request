3ce4da89d4375d5853c7afc7112cf709e3e7baf1
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-06-25 21:50:29 -0400

    Create lldap_graphql class

---
+++ lldap_graphql/const.py
@@ -0,0 +1,6 @@
+"""Constants for lldap_graphql."""
+
+LLDAP_HTTPURL_DEFAULT = "http://lldap:17170"
+HTTP_AUTH_ENDPOINT = "/auth/simple/login"
+HTTP_GRAPHQL_ENDPOINT = "/api/graphql"
+HTTP_REFRESH_ENDPOINT = "/auth/refresh"
